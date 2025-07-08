cd738801e94a1537cfc2531cdc7ae41a70409a8f
surya <suryakant231216@gmail.com>
2025-07-08 14:47:58 +0530

    commit

--- Student-Management-App/src/main/java/com/example/Student_Management_App/StudentRepository.java
+++ Student-Management-App/src/main/java/com/example/Student_Management_App/StudentRepository.java
@@ -1,13 +1,19 @@
 package com.example.Student_Management_App;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
 public class StudentRepository {
     Map<Integer, Student> studentDb = new HashMap<>();
+    Map<Integer, Teacher> teacherDb = new HashMap<>();
+    Map<Integer, List<Integer>> teacherStudentDb = new HashMap<>();
 
     public Student getStudent(int id){
         return studentDb.get(id);
@@ -22,4 +28,62 @@ public class StudentRepository {
 
     }
 
+    public void addTeacher(Teacher teacher) {
+        teacherDb.put(teacher.getId(), teacher);
+    }
+
+    public Teacher getTeacher(String name) {
+        for(Integer teacherId : teacherDb.keySet()){
+            if(teacherDb.get(teacherId).getName().equals(name)){
+                return teacherDb.get(teacherId);
+            }
+        }
+        return null;
+    }
+
+    public List<String> getAllStudents() {
+            List<String> allStudents = new ArrayList<>();
+            for(Integer student : studentDb.keySet()){
+                allStudents.add(studentDb.get(student).getName());
+            }
+            return allStudents;
+    }
+
+    public void deleteTeacher(String name) {
+        int teacherId = -1;
+        for(Integer id : teacherDb.keySet()){
+            if(teacherDb.get(id).getName().equals(name)){
+                teacherId = id;
+                break;
+            }
+        }
+
+        if(teacherId != 1){
+            teacherDb.remove(teacherId);
+            teacherStudentDb.remove(teacherId);
+        }
+    }
+
+    public void addTeacherStudentPair(Integer sId, Integer tId) {
+        if(studentDb.containsKey(sId) && teacherDb.containsKey(tId)){
+            if(!teacherStudentDb.containsKey(tId)){
+                teacherStudentDb.put(tId, new ArrayList<>());
+            }
+            teacherStudentDb.get(tId).add(sId);
+        }
+    }
+
+    public List<String> getStudentByTeacherId(Integer tId) {
+        List<Integer> studentIdList = teacherStudentDb.get(tId);
+        List<String> studentsList = new ArrayList<>();
+
+        if(studentIdList != null) {
+            for (Integer i : studentIdList) {
+                studentsList.add(studentDb.get(i).getName());
+            }
+        }
+
+        return  studentsList;
+
+    }
 }
--- Student-Management-App/src/main/java/com/example/Student_Management_App/StudentService.java
+++ Student-Management-App/src/main/java/com/example/Student_Management_App/StudentService.java
@@ -1,7 +1,11 @@
 package com.example.Student_Management_App;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Service
 public class StudentService {
@@ -21,4 +25,29 @@ public class StudentService {
         return "Student added successfully!";
     }
 
+    public String addTeacher(Teacher teacher) {
+        studentRepository.addTeacher(teacher);
+        return "Teacher added successfully!";
+    }
+
+    public Teacher getTeacher(String name) {
+        return studentRepository.getTeacher(name);
+    }
+
+
+    public List<String> getAllStudents() {
+        return studentRepository.getAllStudents();
+    }
+
+    public void deleteTeacher(String name) {
+        studentRepository.deleteTeacher(name);
+    }
+
+    public void addTeacherStudentPair(Integer sId, Integer tId) {
+        studentRepository.addTeacherStudentPair(sId, tId);
+    }
+
+    public List<String> getStudentByTeacherId(Integer tId) {
+        return studentRepository.getStudentByTeacherId(tId);
+    }
 }
